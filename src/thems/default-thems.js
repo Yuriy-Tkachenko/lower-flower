637d19a4a6291c9b1047b802ab2f3756f54273bf
Yuriy-Tkachenko <apolo16@inbox.ru>
2023-09-19 15:21:37 +0300

    created page thems

---
+++ src/thems/default-thems.js
@@ -0,0 +1,13 @@
+export const defaultTheme = {
+  colorWhite: "#FFFFFF",
+  colorBlack: "#000000",
+  colorElectricGreen: "#43FFD2",
+  colorDarkGreen: "#0F2222",
+  colorGreyPink: "#D978AC",
+  colorExtraPink: "#7D2253",
+  fontFamily: '"Oswald", "Arial", sans-serif',
+  fontSizeDefault: "14px",
+  fontWeightDefault: 400,
+  desktopWidth: "1110px",
+  mobileWidth: "300px"
+}
