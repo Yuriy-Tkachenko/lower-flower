ef20bf949016de9ff90486e3c8b50f26c3f38bd6
Yuriy-Tkachenko <apolo16@inbox.ru>
2023-09-19 15:11:15 +0300

    added images and mocks for main page

---
+++ src/mocks/popular.js
@@ -0,0 +1,33 @@
+import firstFlowers1x from "../assets/first-flowers@1x.jpg";
+import firstFlowers2x from "../assets/first-flowers@2x.jpg";
+import secondFlowers1x from "../assets/second-flowers@1x.jpg";
+import secondFlowers2x from "../assets/second-flowers@2x.jpg";
+import thirdFlowers1x from '../assets/third-flowers@1x.jpg';
+import thirdFlowers2x from '../assets/third-flowers@2x.jpg';
+
+const popular = [
+  {
+    id: 0,
+    title: 'Просто так',
+    newPrice: 2800,
+    oldPrice: 3600,
+    image1x: firstFlowers1x,
+    image2x: firstFlowers2x,
+  },
+  {
+    id: 1,
+    title: 'Прекрасное настроение',
+    newPrice: 2000,
+    oldPrice: 3500,
+    image1x: secondFlowers1x,
+    image2x: secondFlowers2x,
+  },
+  {
+    id: 2,
+    title: 'Лучший день',
+    newPrice: 5500,
+    oldPrice: 7000,
+    image1x: thirdFlowers1x,
+    image2x: thirdFlowers2x,
+  }
+]
